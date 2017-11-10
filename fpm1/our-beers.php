158cb4a148f92c5295ac7dbaff8660629d3ab508
Aleks Itskovich <31596660+ai223@users.noreply.github.com>
2017-11-10 09:27:52 -0500

    Removed placeholder text, added tagline

--- fpm1/our-beers.php
+++ fpm1/our-beers.php
@@ -16,9 +16,9 @@
   ?>
 
 <!-- NEW CODE GOES HERE -->
-
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in urna id velit placerat viverra. Aenean scelerisque rutrum dolor. Suspendisse porta, lacus ut ullamcorper tempor, lacus libero aliquam augue, sit amet egestas ex arcu id metus. Fusce lorem metus, mattis tristique lectus eget, sollicitudin congue nibh. Aliquam sed felis lacus. Quisque vulputate porttitor mi et gravida. Ut vulputate eros vitae bibendum laoreet. Cras faucibus egestas erat venenatis interdum. Sed quis mi molestie, venenatis ligula vitae, malesuada nisi. Morbi vestibulum a risus sit amet ullamcorper. Nunc metus erat, dignissim eget feugiat vel, vehicula ut diam. Integer feugiat leo magna, malesuada varius arcu rutrum non. Etiam quis scelerisque risus. Donec facilisis erat eu sem vulputate interdum. Fusce consequat ante eros, in porta nulla fermentum dignissim.</p>
-
+  <div id="tagline">
+    <h2>Ethically-sourced and home-brewed, our ales are a smooth and delicious drinking experience.</h2>
+  </div>
 <!-- NEW CODE ENDS HERE -->
 
 
